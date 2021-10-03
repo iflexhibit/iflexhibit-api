3414ece8371ded80fbda4f6ade6405ea61a121b9
John Paul Ong <jpong5202@gmail.com>
2021-10-03 18:47:17 +0800

    Update sql commands for users

---
+++ src/sql/users/find_email_one.sql
@@ -0,0 +1,4 @@
+SELECT user_id FROM users
+WHERE email LIKE $1
+
+-- SELECT user_id FROM users WHERE email LIKE $1
--- src/sql/users/insert_one.sql
+++ src/sql/users/insert_one.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, given_name, family_name, email, profile_image)
+INSERT INTO users (username, given_name, family_name, email, avatar_image)
 VALUES ($1,$2,$3,$4,$5)
 RETURNING user_id
 
--- INSERT INTO users (username, given_name, family_name, email, profile_image) VALUES ($1,$2,$3,$4,$5) RETURNING user_id
+-- INSERT INTO users (username, given_name, family_name, email, avatar_image) VALUES ($1,$2,$3,$4,$5) RETURNING user_id
