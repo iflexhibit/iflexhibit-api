2c5b333b9a61b55e12a4caab968ceacc110bf46f
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 22:20:12 +0800

    Add CREATE TABLE preferences

--- src/repositories/sql/userTypes/_table.sql
+++ src/repositories/sql/userTypes/_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE usertypes (
     "usertype_id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "permission_id" INT NOT NULL REFERENCES permissions(permission_id) 0,
+    "permission_id" INT NOT NULL REFERENCES permissions(permission_id) DEFAULT 0,
     "usertype_title" VARCHAR(20) NOT NULL DEFAULT 'NO ACCESS',
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
--- src/repositories/sql/users/_table.sql
+++ src/repositories/sql/users/_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     "user_id" SERIAL PRIMARY KEY UNIQUE,
-    "usertype_id" INT NOT NULL FOREIGN KEY REFERENCES userTypes(usertype_id),
+    "usertype_id" INT NOT NULL REFERENCES usertypes(usertype_id),
     "username" TEXT NOT NULL UNIQUE,
     "given_name" TEXT DEFAULT NULL,
     "family_name" TEXT DEFAULT NULL,
@@ -13,4 +13,4 @@ CREATE TABLE users (
     "show_contact" BOOLEAN DEFAULT FALSE,
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
-)
+);
