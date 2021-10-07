f98c94703a76fd989b64d02b04ac2179855724ce
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 22:05:11 +0800

    Add CREATE TABLE poststatus

--- src/repositories/sql/postStatus/_table.sql
+++ src/repositories/sql/postStatus/_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE poststatus (
     "status_id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "status_title" VARCHAR(10) NOT NULL DEFAULT "Pending",
+    "status_title" VARCHAR(10) NOT NULL DEFAULT 'Pending',
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
-)
+);
