27dc95c0c2de2d83709688a15b2499fb4c9c60af
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 23:02:13 +0800

    Revised CREATE TABLE bans

--- src/repositories/sql/bans/_table.sql
+++ src/repositories/sql/bans/_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE bans(
     "ban_id" SMALLSERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "report_id" INT NOT NULL FOREIGN KEY REFERENCES reports(report_id),
-    "target_id" INT NOT NULL FOREIGN KEY REFERENCES users(user_id),
+    "report_id" INT NOT NULL REFERENCES reports(report_id),
+    "target_id" INT NOT NULL REFERENCES users(user_id),
     "user_id" INT NOT NULL FOREIGN KEY REFERENCES users(user_id),
     "offense_id" INT NOT NULL FOREIGN KEY REFERENCES offenses(offense_id),
     "ban_note" TEXT NOT NULL,
