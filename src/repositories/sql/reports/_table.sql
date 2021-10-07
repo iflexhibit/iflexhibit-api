c45f93cb20d2c36b97963b2e18ab9885ab34a42b
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 22:54:30 +0800

    Add CREATE TABLE reposrts

--- src/repositories/sql/reports/_table.sql
+++ src/repositories/sql/reports/_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE reports (
     "report_id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "target_user_id" INT NOT NULL,
-    "target_post_id" INT NULL NULL,
-    "target_comment_id" INT NULL NULL,
-    "user_id" INT NOT NULL FOREIGN KEY REFERENCES users(user_id),
-    "offense_id" SMALLINT NOT NULL FOREIGN KEY REFERENCES offenses(offense_id),
-    "report_note" TEXT NULL ,
+    "target_user_id" INT NOT NULL REFERENCES users(user_id),
+    "target_post_id" INT DEFAULT NULL REFERENCES posts(post_id) NULL,
+    "target_comment_id" INT DEFAULT NULL REFERENCES comments(comment_id) NULL,
+    "user_id" INT NOT NULL REFERENCES users(user_id),
+    "offense_id" SMALLINT NOT NULL REFERENCES offenses(offense_id),
+    "report_note" TEXT DEFAULT NULL,
     "is_deleted" BOOLEAN NOT NULL DEFAULT FALSE,
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
-)
+);
