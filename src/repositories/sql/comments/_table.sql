cc4c9cb9ace9fdedbd017b4601e222d95bc085ef
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 22:37:08 +0800

    Add CREATE TABLE comments

--- src/repositories/sql/comments/_table.sql
+++ src/repositories/sql/comments/_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE comments (
     "comment_id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "user_id" INT NOT NULL FOREIGN KEY REFERENCES users(user_id),
-    "post_id"INT NOT NULL FOREIGN KEY REFERENCES posts(post_id),
+    "user_id" INT NOT NULL REFERENCES users(user_id),
+    "post_id"INT NOT NULL REFERENCES posts(post_id),
     "comment_body" TEXT NOT NULL,
     "is_disabled" BOOLEAN NOT NULL DEFAULT FALSE,
     "is_deleted" BOOLEAN NOT NULL DEFAULT FALSE,
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
-)
+);
