df37724b28ee6930ba42c8db89dbf3ff819cb636
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 22:59:10 +0800

    Revised userpost table

--- src/repositories/sql/userPost/_table.sql
+++ src/repositories/sql/userPost/_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE userpost (
     "userpost_id" BIGSERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "user_id" INT NOT NULL FOREIGN KEY REFERENCES users(users_id),
-    "post_id" INT NOT NULL FOREIGN KEY REFERENCES posts(post_id),
+    "user_id" INT NOT NULL REFERENCES users(user_id),
+    "post_id" INT NOT NULL REFERENCES posts(post_id),
     "is_liked" BOOLEAN NOT NULL DEFAULT FALSE,
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
-)
+);
