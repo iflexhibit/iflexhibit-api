c113d653f88435dd0b1b492f75e1d9a5669cd309
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-07 22:11:11 +0800

    Add CREATE TABLE usertypes

--- src/repositories/sql/userTypes/_table.sql
+++ src/repositories/sql/userTypes/_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE usertypes (
     "usertype_id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "permission_id" INT NOT NULL FOREIGN KEY REFERENCES permissions(permission_id) 0,
-    "usertype_title" VARCHAR(20) NOT NULL DEFAULT "NO ACCESS",
+    "permission_id" INT NOT NULL REFERENCES permissions(permission_id) 0,
+    "usertype_title" VARCHAR(20) NOT NULL DEFAULT 'NO ACCESS',
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
-)
+);
