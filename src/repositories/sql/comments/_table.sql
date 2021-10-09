1ffa0b24c1adaf2dfddda923015d0b82b295fa85
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-09 14:11:11 +0800

    resolves post_id spacing issue

--- src/repositories/sql/comments/_table.sql
+++ src/repositories/sql/comments/_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE comments (
     "comment_id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
     "user_id" INT NOT NULL REFERENCES users(user_id),
-    "post_id"INT NOT NULL REFERENCES posts(post_id),
+    "post_id" INT NOT NULL REFERENCES posts(post_id),
     "comment_body" TEXT NOT NULL,
     "is_disabled" BOOLEAN NOT NULL DEFAULT FALSE,
     "is_deleted" BOOLEAN NOT NULL DEFAULT FALSE,
