8732cafa8ab715822d62581ba7c3a13bdf9dc513
John Paul Ong <jpong5202@gmail.com>
2021-10-09 21:07:24 +0800

    Remove duplicate sql folders

--- src/repositories/sql/user-post/_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE userpost (
-    "userpost_id" BIGSERIAL NOT NULL PRIMARY KEY UNIQUE,
-    "user_id" INT NOT NULL REFERENCES users(user_id),
-    "post_id" INT NOT NULL REFERENCES posts(post_id),
-    "is_liked" BOOLEAN NOT NULL DEFAULT FALSE,
-    "created_at" TIMESTAMPTZ DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ DEFAULT NOW()
-);
