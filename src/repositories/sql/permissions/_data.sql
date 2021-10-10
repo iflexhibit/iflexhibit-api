cb594ffe05f56bcfa811d724ae10306d64e92422
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-10 18:49:19 +0800

    adds moderator access

--- src/repositories/sql/permissions/_data.sql
+++ src/repositories/sql/permissions/_data.sql
@@ -1,2 +1,6 @@
+-- member permissions
 INSERT INTO permissions(submit_post, comment_post, moderator_access, admin_access)
 VALUES (TRUE, TRUE, FALSE, FALSE);
+-- moderator
+INSERT INTO permissions(submit_post, comment_post, moderator_access, admin_access)
+VALUES (TRUE, TRUE, TRUE, FALSE);
