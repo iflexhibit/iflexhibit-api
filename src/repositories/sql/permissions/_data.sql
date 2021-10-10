35a47d6a77713ce8b489a5458acbf870fdc4ae0f
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-10 18:51:28 +0800

    adds admin access

--- src/repositories/sql/permissions/_data.sql
+++ src/repositories/sql/permissions/_data.sql
@@ -4,3 +4,9 @@ VALUES (TRUE, TRUE, FALSE, FALSE);
 -- moderator
 INSERT INTO permissions(submit_post, comment_post, moderator_access, admin_access)
 VALUES (TRUE, TRUE, TRUE, FALSE);
+-- administrator
+INSERT INTO permissions(submit_post, comment_post, moderator_access, admin_access)
+VALUES (TRUE, TRUE, TRUE, TRUE);
+-- banned 
+INSERT INTO permissions(submit_post, comment_post, moderator_access, admin_access)
+VALUES (FALSE, FALSE, FALSE, FALSE);
