a92c4b1a4b1e31dc0215f9939c64773d8581b170
John Paul Ong <jpong5202@gmail.com>
2021-10-10 20:23:20 +0800

    Update usertypes permission_id

--- src/repositories/sql/usertypes/_data.sql
+++ src/repositories/sql/usertypes/_data.sql
@@ -1,12 +1,12 @@
 -- member
 INSERT INTO usertypes (permission_id, usertype_title)
-VALUES (0, 'member');
+VALUES (1, 'member');
 -- moderator
 INSERT INTO usertypes (permission_id, usertype_title)
-VALUES (1, 'moderator');
+VALUES (2, 'moderator');
 -- administrator
 INSERT INTO usertypes (permission_id, usertype_title)
-VALUES (2, 'administrator');
+VALUES (3, 'administrator');
 -- banned
 INSERT INTO usertypes (permission_id, usertype_title)
-VALUES (3, 'banned');
+VALUES (4, 'banned');
