3e5c5d493570f842c541c4125da61de67bb58efa
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-10 19:14:49 +0800

    Adds INSERT INTO command for moderator

--- src/repositories/sql/usertypes/_data.sql
+++ src/repositories/sql/usertypes/_data.sql
@@ -1,3 +1,6 @@
 -- member
 INSERT INTO usertypes (permission_id, usertype_title)
 VALUES (0, 'member');
+-- moderator
+INSERT INTO usertypes (permission_id, usertype_title)
+VALUES (1, 'moderator');
