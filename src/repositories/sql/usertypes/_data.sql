4fcfe26a773482d5f6c6592c766d6ea1f00e9d81
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-10 19:15:14 +0800

    Adds INSERT INTO command for admin

--- src/repositories/sql/usertypes/_data.sql
+++ src/repositories/sql/usertypes/_data.sql
@@ -7,3 +7,6 @@ VALUES (1, 'moderator');
 -- administrator
 INSERT INTO usertypes (permission_id, usertype_title)
 VALUES (2, 'administrator');
+-- banned
+INSERT INTO usertypes (permission_id, usertype_title)
+VALUES (3, 'banned');
