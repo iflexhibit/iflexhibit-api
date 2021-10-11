6e6563b12b5d3ff2dc91aa4d9687b5e8094e6315
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-11 21:24:40 +0800

    adds fetch profile

--- src/repositories/sql/users/fetch_profile.sql
+++ src/repositories/sql/users/fetch_profile.sql
@@ -1,22 +1,2 @@
-SELECT
-    user_id,
-    CASE WHEN show_name=FALSE THEN NULL
-        ELSE given_name,
-    CASE WHEN permission_id=0 THEN 'member'
-         WHEN permission_id=1 THEN 'moderator'
-         WHEN permission_id=2 THEN 'administrator'
-         WHEN permission_id=3 THEN 'banned'
-        ELSE,
-    username,
-    CASE WHEN show_name=FALSE THEN NULL
-        ELSE given_name,
-    CASE WHEN show_name=FALSE THEN NULL
-        ELSE family_name, 
-    CASE WHEN show_email=FALSE THEN NULL
-        ELSE email,
-    CASE WHEN show_contact=FALSE THEN NULL
-        ELSE contact,
-    avatar_image,
-    background_image,
-    created_at
-FROM users, usertypes WHERE user_id=$1;
+SELECT * FROM users
+JOIN usertypes ON users.usertype_id=usertypes.usertype_id WHERE user_id=$1;
