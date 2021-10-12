37d5a56d8b506e13efc7b3e3af42ef0aad70705c
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-12 17:29:46 +0800

    added new fields in fetch profile sql command

--- src/repositories/sql/users/fetch_profile.sql
+++ src/repositories/sql/users/fetch_profile.sql
@@ -1,20 +1,20 @@
-SELECT * FROM users
-JOIN usertypes ON users.usertype_id=usertypes.usertype_id WHERE user_id=$1;
-
 SELECT user_id,
 B.usertype_title,
 username,
-CASE WHEN show_name=FALSE THEN NULL
+CASE WHEN show_name = FALSE THEN NULL
         ELSE given_name
         END,
-CASE WHEN show_name=FALSE THEN NULL
+    CASE WHEN show_name = FALSE THEN NULL
         ELSE family_name
         END,
-CASE WHEN show_email=FALSE THEN NULL
+    CASE WHEN show_email = FALSE THEN NULL
         ELSE email
         END,
-CASE WHEN show_contact=FALSE THEN NULL
+    CASE WHEN show_contact = FALSE THEN NULL
         ELSE contact
-       END
+       END,
+    A.avatar_image,
+    A.background_image,
+    A.created_at
 FROM users
-A INNER JOIN usertypes ON A.usertype_id=B.usertype_id WHERE user_id=$1;
+A INNER JOIN usertypes B ON A.usertype_id=B.usertype_id WHERE user_id=$1;
