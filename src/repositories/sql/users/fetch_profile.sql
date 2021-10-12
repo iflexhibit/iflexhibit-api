4cc17fb66c61c161e7f348374d4caeb0c8de1f2b
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-12 11:25:17 +0800

    adds fetch profile sql command

--- src/repositories/sql/users/fetch_profile.sql
+++ src/repositories/sql/users/fetch_profile.sql
@@ -1,2 +1,20 @@
 SELECT * FROM users
 JOIN usertypes ON users.usertype_id=usertypes.usertype_id WHERE user_id=$1;
+
+SELECT user_id,
+B.usertype_title,
+username,
+CASE WHEN show_name=FALSE THEN NULL
+        ELSE given_name
+        END,
+CASE WHEN show_name=FALSE THEN NULL
+        ELSE family_name
+        END,
+CASE WHEN show_email=FALSE THEN NULL
+        ELSE email
+        END,
+CASE WHEN show_contact=FALSE THEN NULL
+        ELSE contact
+       END
+FROM users
+A INNER JOIN usertypes ON A.usertype_id=B.usertype_id WHERE user_id=$1;
