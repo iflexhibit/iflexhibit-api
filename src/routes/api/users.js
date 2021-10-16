1926e0832c70ccb755f6f4348cb44c3370c0f748
John Paul Ong <jpong5202@gmail.com>
2021-10-16 18:07:38 +0800

    Add logging for sessionID

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,6 +4,7 @@ const UserRepository = require("../../repositories/UserRepository");
 const router = express.Router();
 
 router.get("/user", async (req, res) => {
+  console.log(req.sessionID);
   if (!req.user) {
     return res.status(401).json({ status: 401 });
   }
