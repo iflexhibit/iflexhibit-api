e79b95fdbd28a86ce3189bb1dc28e990b689d263
John Paul Ong <jpong5202@gmail.com>
2021-10-16 12:55:13 +0800

    Rename route from /profile to /user

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -3,13 +3,13 @@ const UserRepository = require("../../repositories/UserRepository");
 
 const router = express.Router();
 
-router.get("/profile", async (req, res) => {
+router.get("/user", async (req, res) => {
   if (!req.user) {
     return res.sendStatus(401);
   }
   try {
     const user = await UserRepository.fetchProfile(req.user.userId);
-    if (!user) return res.sendStatus(404);
+    if (!user) return res.sendStatus(401);
     return res.status(200).json({ user });
   } catch (error) {
     return res.sendStatus(500);
