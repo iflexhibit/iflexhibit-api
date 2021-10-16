ca6f6b4eb83a66bc182fcb5b18d7a534d8298d65
John Paul Ong <jpong5202@gmail.com>
2021-10-16 16:44:49 +0800

    Add end to response

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -10,9 +10,9 @@ router.get("/user", async (req, res) => {
   try {
     const user = await UserRepository.fetchProfile(req.user.userId);
     if (!user) return res.status(401).json({ status: 401 });
-    return res.status(200).json({ status: 200, user });
+    return res.status(200).json({ status: 200, user }).end();
   } catch (error) {
-    return res.status(500).json({ status: 500 });
+    return res.status(500).json({ status: 500 }).end();
   }
 });
 
