21fcf339a6a2f30638dd8dc831340b9cd895acd5
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-16 21:39:02 +0800

    adds INSERT INTO sql commands

---
+++ src/repositories/sql/poststatus/_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO poststatus (status_title)
+VALUE ("pending");
+
+INSERT INTO poststatus (status_title)
+VALUE ("approved");
+
+INSERT INTO poststatus (status_title)
+VALUE ("rejected");
+
+INSERT INTO poststatus (status_title)
+VALUE ("disabled");
