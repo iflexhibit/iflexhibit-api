43657a4076ac6ce81784a3c2c261bb7fb5103100
John Paul Ong <jpong5202@gmail.com>
2021-10-16 19:03:00 +0800

    Update cookie configuration

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.use(
       pool,
       pruneSessionInterval: 60,
     }),
-    cookie: { maxAge: db.COOKIE_MAXAGE, secure: true },
+    cookie: { maxAge: db.COOKIE_MAXAGE, secure: false },
     secret: db.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
