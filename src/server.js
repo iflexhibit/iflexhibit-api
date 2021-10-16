a6eec2d8cb73c6291bac7f256ea7e26ffddd9a6a
John Paul Ong <jpong5202@gmail.com>
2021-10-16 19:13:46 +0800

    Set samesite to strict

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.use(
       pool,
       pruneSessionInterval: 60,
     }),
-    cookie: { maxAge: db.COOKIE_MAXAGE, secure: false, sameSite: "none" },
+    cookie: { maxAge: db.COOKIE_MAXAGE, secure: false, sameSite: "strict" },
     secret: db.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
