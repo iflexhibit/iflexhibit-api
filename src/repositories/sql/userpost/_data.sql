86d188cfc65f536e20c80a8e9ad848bbd10d9f5c
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-17 19:03:42 +0800

    Adds an entry of a unliked post

--- src/repositories/sql/userpost/_data.sql
+++ src/repositories/sql/userpost/_data.sql
@@ -4,3 +4,8 @@ VALUES (5, 20, TRUE);
 INSERT INTO userpost (user_id, post_id, is_liked)
 VALUES (5, 19, TRUE);
 
+INSERT INTO userpost (user_id, post_id, is_liked)
+VALUES (5, 18, FALSE);
+
+-- please change the values of the user_id and post_id to match 
+-- you corresponding values
