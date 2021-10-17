cbca594682be27f74a1c33c5abdf7d9595031282
LyndonTomas <61931218+LyndonTomas@users.noreply.github.com>
2021-10-17 04:11:03 -0700

    Adds INSERT INTO values to userpost (#43)
    Add INSERT INTO values to userpost

---
+++ src/repositories/sql/userpost/_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO userpost (user_id, post_id, is_liked)
+VALUES (1, 20, TRUE);
+
+INSERT INTO userpost (user_id, post_id, is_liked)
+VALUES (1, 19, TRUE);
+
+INSERT INTO userpost (user_id, post_id, is_liked)
+VALUES (1, 18, FALSE);
+
+-- please change the values of the user_id and post_id to match 
+-- you corresponding values
