75b4dc4a22b0650bfd97db250eaee6491a898a54
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-17 10:57:02 +0800

    added pending entry

--- src/repositories/sql/posts/_data.sql
+++ src/repositories/sql/posts/_data.sql
@@ -5,5 +5,7 @@
 -- 1 approved with is_deleted=TRUE DONE
 -- 6 approved with is_deleted=FALSE (the only visible posts to guest)
 
-
+-- pending
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (1, 5, 'post-sample-pending', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
 
