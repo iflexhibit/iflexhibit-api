f1dccb84f6d297b9e62efb4b95c4cacf292d1a63
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-17 19:07:53 +0800

    Changes user_id back to 1

--- src/repositories/sql/userpost/_data.sql
+++ src/repositories/sql/userpost/_data.sql
@@ -1,11 +1,11 @@
 INSERT INTO userpost (user_id, post_id, is_liked)
-VALUES (5, 20, TRUE);
+VALUES (1, 20, TRUE);
 
 INSERT INTO userpost (user_id, post_id, is_liked)
-VALUES (5, 19, TRUE);
+VALUES (1, 19, TRUE);
 
 INSERT INTO userpost (user_id, post_id, is_liked)
-VALUES (5, 18, FALSE);
+VALUES (1, 18, FALSE);
 
 -- please change the values of the user_id and post_id to match 
 -- you corresponding values
