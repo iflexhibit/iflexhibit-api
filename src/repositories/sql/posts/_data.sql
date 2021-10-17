672aa757bd9b3c5769f29bfc7f96b5479c823e5c
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-17 10:59:26 +0800

    added 6 approved entries

--- src/repositories/sql/posts/_data.sql
+++ src/repositories/sql/posts/_data.sql
@@ -20,3 +20,22 @@ VALUES (3, 5, 'post-sample-rejected', 'This post is aimed to populate the table 
 -- deleted
 INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
 VALUES (2, 5, 'post-sample-approved-is deleted', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', TRUE);
+
+-- approved and visible to guest
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (2, 5, 'post-sample-approved', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
+
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (2, 5, 'post-sample-approved', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
+
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (2, 5, 'post-sample-approved', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
+
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (2, 5, 'post-sample-approved', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
+
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (2, 5, 'post-sample-approved', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
+
+INSERT INTO posts (status_id, user_id, post_title, post_body, post_image, post_video, post_tags, is_deleted)
+VALUES (2, 5, 'post-sample-approved', 'This post is aimed to populate the table with variuos data.', 'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', NULL, 'Photography,Portrait', FALSE);
