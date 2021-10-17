095a57292851503c285465c1ccdb755826d0b757
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-17 10:56:38 +0800

    listed TODOs

---
+++ src/repositories/sql/posts/_data.sql
@@ -0,0 +1,9 @@
+-- List of todo's
+-- 1 disabled DONE
+-- 1 pending DONE
+-- 1 rejected DONE
+-- 1 approved with is_deleted=TRUE DONE
+-- 6 approved with is_deleted=FALSE (the only visible posts to guest)
+
+
+
