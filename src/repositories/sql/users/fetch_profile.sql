e32a5d6ecc25200001a0db4c669e0e2dcb58d5b5
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-24 11:31:18 +0800

    fixed the wrong file mb

--- src/repositories/sql/users/fetch_profile.sql
+++ src/repositories/sql/users/fetch_profile.sql
@@ -1,21 +1,20 @@
--- Will keep this as basis for now
--- SELECT user_id,
--- B.usertype_title,
--- username,
--- CASE WHEN show_name = FALSE THEN NULL
---         ELSE given_name
---         END,
---     CASE WHEN show_name = FALSE THEN NULL
---         ELSE family_name
---         END,
---     CASE WHEN show_email = FALSE THEN NULL
---         ELSE email
---         END,
---     CASE WHEN show_contact = FALSE THEN NULL
---         ELSE contact
---        END,
---     A.avatar_image,
---     A.background_image,
---     A.created_at
--- FROM users
--- A INNER JOIN usertypes B ON A.usertype_id=B.usertype_id WHERE user_id=$1;
+SELECT user_id,
+B.usertype_title,
+username,
+CASE WHEN show_name = FALSE THEN NULL
+        ELSE given_name
+        END,
+    CASE WHEN show_name = FALSE THEN NULL
+        ELSE family_name
+        END,
+    CASE WHEN show_email = FALSE THEN NULL
+        ELSE email
+        END,
+    CASE WHEN show_contact = FALSE THEN NULL
+        ELSE contact
+       END,
+    A.avatar_image,
+    A.background_image,
+    A.created_at
+FROM users
+A INNER JOIN usertypes B ON A.usertype_id=B.usertype_id WHERE user_id=$1;
