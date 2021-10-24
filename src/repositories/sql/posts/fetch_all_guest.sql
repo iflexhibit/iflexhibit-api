5f3cf42758556a0d7d54d5c9efbacdcf22bb3cef
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-24 11:39:20 +0800

    made the old code as comments

--- src/repositories/sql/posts/fetch_all_guest.sql
+++ src/repositories/sql/posts/fetch_all_guest.sql
@@ -1,29 +1,30 @@
-SELECT 
-posts.post_id,
-poststatus.status_title,
-users.username,
-posts.post_title,
-posts.post_image,
-posts.post_tags,
-(
-    SELECT COUNT(*)
-    FILTER (WHERE posts.user_id=users.user_id)
-    FROM userpost 
-) AS views_count,
-(
-    SELECT COUNT(*)
-    FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
-    FROM userpost
-) AS likes_count,
-(
-    SELECT COUNT(*)
-    FILTER (WHERE posts.post_id=comments.post_id AND comments.is_disabled=FALSE AND comments.is_deleted=FALSE)
-    FROM comments
-) AS comments_count,
-posts.created_at
-FROM posts
-INNER JOIN comments ON posts.post_id=comments.post_id
-INNER JOIN poststatus ON posts.status_id=poststatus.status_id
-INNER JOIN users ON posts.user_id=users.user_id
-WHERE posts.status_id=2 AND posts.is_deleted=FALSE
-;
+-- SELECT 
+-- posts.post_id,
+-- poststatus.status_title,
+-- users.username,
+-- posts.post_title,
+-- posts.post_image,
+-- posts.post_tags,
+-- (
+--     SELECT COUNT(*)
+--     FILTER (WHERE posts.user_id=users.user_id)
+--     FROM userpost 
+-- ) AS views_count,
+-- (
+--     SELECT COUNT(*)
+--     FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
+--     FROM userpost
+-- ) AS likes_count,
+-- (
+--     SELECT COUNT(*)
+--     FILTER (WHERE posts.post_id=comments.post_id AND comments.is_disabled=FALSE AND comments.is_deleted=FALSE)
+--     FROM comments
+-- ) AS comments_count,
+-- posts.created_at
+-- FROM posts
+-- INNER JOIN comments ON posts.post_id=comments.post_id
+-- INNER JOIN poststatus ON posts.status_id=poststatus.status_id
+-- INNER JOIN users ON posts.user_id=users.user_id
+-- WHERE posts.status_id=2 AND posts.is_deleted=FALSE
+-- GROUP BY posts.post_id ORDER BY posts.post_id
+-- ;
