c4f2463327a6a1ccf301bebb6c400d91aaf9e398
LyndonTomas <201801180@iacademy.edu.ph>
2021-10-24 11:28:36 +0800

    made the old code as comments

--- src/repositories/sql/posts/fetch_all_guest.sql
+++ src/repositories/sql/posts/fetch_all_guest.sql
@@ -4,7 +4,7 @@ poststatus.status_title,
 users.username,
 posts.post_title,
 posts.post_image,
-post_tags,
+posts.post_tags,
 (
     SELECT COUNT(*)
     FILTER (WHERE posts.user_id=users.user_id)
@@ -12,16 +12,18 @@ post_tags,
 ) AS views_count,
 (
     SELECT COUNT(*)
-    FILTER (WHERE is_liked AND posts.post_id=userpost.post_id)
+    FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
     FROM userpost
 ) AS likes_count,
 (
     SELECT COUNT(*)
+    FILTER (WHERE posts.post_id=comments.post_id AND comments.is_disabled=FALSE AND comments.is_deleted=FALSE)
     FROM comments
 ) AS comments_count,
 posts.created_at
 FROM posts
+INNER JOIN comments ON posts.post_id=comments.post_id
 INNER JOIN poststatus ON posts.status_id=poststatus.status_id
 INNER JOIN users ON posts.user_id=users.user_id
-WHERE posts.status_id=2 AND is_deleted=FALSE
+WHERE posts.status_id=2 AND posts.is_deleted=FALSE
 ;
