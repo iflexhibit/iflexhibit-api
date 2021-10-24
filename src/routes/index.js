56f6ae9db781328ed7a9626abd2838148692657d
John Paul Ong <jpong5202@gmail.com>
2021-10-24 14:48:46 +0800

    Add fetch posts API endpoint for guest view

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.send("API"));
 router.use("/users", require("./api/users"));
+router.use("/posts", require("./api/posts"));
 router.use("/auth", require("./api/auth"));
 
 module.exports = router;
