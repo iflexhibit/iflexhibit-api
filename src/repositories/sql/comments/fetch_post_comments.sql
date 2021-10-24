db5e682783fa095dd22437b4d4e51e62fafae619
LyndonTomas <61931218+LyndonTomas@users.noreply.github.com>
2021-10-24 05:11:24 -0700

    Adds sql command for fetch post comments (#51)
    * Adds sql command for fetch post comments
    
    * updated the vlue of comments.post_id to

---
+++ src/repositories/sql/comments/fetch_post_comments.sql
@@ -0,0 +1,13 @@
+SELECT 
+comments.comment_id,
+users.username,
+users.avatar_image,
+CASE WHEN is_disabled=FALSE THEN comments.comment_body
+ELSE 'This comment has been disabled.'
+END AS comment_body,
+comments.is_disabled,
+comments.created_at
+FROM comments
+JOIN users ON comments.user_id=users.user_id
+JOIN posts ON comments.post_id=posts.post_id
+WHERE comments.post_id=$1;
