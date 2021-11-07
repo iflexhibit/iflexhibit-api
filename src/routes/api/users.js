4564e57696ce18f9eed10dea42fbcc218ebb18db
John Paul Ong <jpong5202@gmail.com>
2021-11-07 12:22:54 +0800

    Add fetch profile API endpoint

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const auth = require("../../middlware/auth");
-const { insertComment } = require("../../repositories/UserRepository");
+const {
+  insertComment,
+  fetchProfile,
+} = require("../../repositories/UserRepository");
 const router = express.Router();
 
 router.get("/user", auth, async (req, res) => {
   return res.status(200).json({ status: 200, user: req.user });
+});
+
+router.get("/user/:id", auth, async (req, res) => {
+  if (isNaN(req.params.id))
+    return res.status(400).json({ status: 400, msg: "Bad request" });
+  try {
+    const user = await fetchProfile(req.params.id);
+    if (!user) return res.status(404).json({ status: 404, msg: "Not found" });
+    return res.status(200).json({ status: 200, user });
+  } catch (error) {
+    return res.status(500).json({ status: 500, msg: "Something went wrong" });
+  }
 });
 
 router.post("/comment", auth, async (req, res) => {
