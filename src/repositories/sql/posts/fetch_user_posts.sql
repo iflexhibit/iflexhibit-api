e01e011b285b2fdbc04381455bae0e51a039ea80
LyndonTomas <201801180@iacademy.edu.ph>
2021-11-08 20:51:21 +0800

    updated fetch user posts sql command

--- src/repositories/sql/posts/fetch_user_posts.sql
+++ src/repositories/sql/posts/fetch_user_posts.sql
@@ -25,4 +25,4 @@ SELECT
 FROM posts
 JOIN users ON posts.user_id = users.user_id
 JOIN poststatus ON posts.status_id = poststatus.status_id
-WHERE posts.status_id = 2 AND posts.is_deleted = FALSE AND posts.user_id = $1;
+WHERE posts.status_id = 2 AND posts.is_deleted = FALSE AND posts.user_id = $1 ORDER BY posts.created_at DESC;
