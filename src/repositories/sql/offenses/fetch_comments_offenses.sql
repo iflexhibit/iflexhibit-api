4dd97f52da9d408376f41f07e496d5bfbbd2299b
LyndonTomas <201801180@iacademy.edu.ph>
2021-11-14 20:53:21 +0800

    Updates fetch comments offenses

--- src/repositories/sql/offenses/fetch_comments_offenses.sql
+++ src/repositories/sql/offenses/fetch_comments_offenses.sql
@@ -1,4 +1,5 @@
 SELECT 
-    offense_code, offense_title 
+    offense_code, offense_title, ban_time, offense_id
 FROM offenses
-WHERE offense_type='c';
+WHERE offense_type='c'
+ORDER BY offense_code;
