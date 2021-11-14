0b6b172efa639e0ccffb475d35d5217de29a62a1
LyndonTomas <201801180@iacademy.edu.ph>
2021-11-14 18:26:21 +0800

    updates the dynamic data in report_user.sql

--- src/repositories/sql/reports/report_user.sql
+++ src/repositories/sql/reports/report_user.sql
@@ -1,6 +1,6 @@
 INSERT INTO reports 
     (target_user_id, target_post_id, target_comment_id, user_id, offense_id, report_note)
 VALUES
-    ($1, NULL, NULL, $4, $5, $6);
+    ($1, NULL, NULL, $2, $3, $4);
 
 -- user to user
