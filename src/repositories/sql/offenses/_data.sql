bf1188faad8b4ab1086f084f4f5ff6f549c3978e
LyndonTomas <201801180@iacademy.edu.ph>
2021-11-14 10:58:06 +0800

    updates _data.sql

--- src/repositories/sql/offenses/_data.sql
+++ src/repositories/sql/offenses/_data.sql
@@ -1,239 +1,198 @@
--- Minor (u)
--- 8.1.1
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    ( offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.1', 'Using foul language or gestures.' , 'u', '07');
+    ( 'Using foul language or gestures.' , 'u', '07');
 
--- 8.1.2
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.2', 'Showing disrespect to National Symbols.' , 'u', '07');
+    ('Showing disrespect to National Symbols.' , 'u', '07');
 
--- 8.1.3
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.3', 'Showing inappropriate behavior.' , 'u', '07');
+    ('Showing inappropriate behavior.' , 'u', '07');
 
--- 8.1.4
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.4', 'Inappropriate display of intimacy.' , 'u', '07');
+    ('Inappropriate display of intimacy.' , 'u', '07');
 
--- Major (u)
--- 8.2.1
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.1', 'Recruiting to any fraternities and/or sororities.' , 'u', '14');
+    ('Recruiting to any fraternities and/or sororities.' , 'u', '14');
 
--- 8.2.2
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.2', 'Any act/form of cheating.' , 'u', '14');
+    ('Any act/form of cheating.' , 'u', '14');
 
--- 8.2.3
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.3', 'Acts of immoral conduct and disrespect.' , 'u', '14');
+    ('Acts of immoral conduct and disrespect.' , 'u', '14');
 
--- 8.2.4
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.4', 'Blatantly disregarding approved school policies.' , 'u', '14');
+    ('Blatantly disregarding approved school policies.' , 'u', '14');
 
--- 8.2.5
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.5', 'Acts of dishonor to the school.' , 'u', '14');
+    ('Acts of dishonor to the school.' , 'u', '14');
 
--- 8.2.6
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.6', 'Acts of dishonor to the school.' , 'u', '14');
+    ('False representation as student organizations.' , 'u', '14');
 
--- 8.2.7
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.7', 'Commiting act of cyberbullying.' , 'u', '14');
+    ('Commiting act of cyberbullying.' , 'u', '14');
 
--- 8.2.8
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.8', 'Acts of Intellectual Property theft.' , 'u', '14');
+    ('Acts of Intellectual Property theft.' , 'u', '14');
 
--- 8.2.9
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.9', 'False claim of Intellectual Property.' , 'u', '14');
+    ('False claim of Intellectual Property.' , 'u', '14');
 
--- Minor (c)
--- 8.1.1
-INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
-VALUES
-    ('8.1.1', 'Using foul language or gestures.' , 'c', '07');
 
--- 8.1.2
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.2', 'Showing disrespect to National Symbols.' , 'c', '07');
+    ('Using foul language or gestures.' , 'c', '07');
 
--- 8.1.3
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.3', 'Showing inappropriate behavior.' , 'c', '07');
+    ('Showing disrespect to National Symbols.' , 'c', '07');
 
--- 8.1.4
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.4', 'Inappropriate display of intimacy.' , 'c', '07');
+    ('Showing inappropriate behavior.' , 'c', '07');
 
--- Major (c)
--- 8.2.1
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.1', 'Recruiting to any fraternities and/or sororities.' , 'c', '14');
+    ('Inappropriate display of intimacy.' , 'c', '07');
 
--- 8.2.2
-INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
-VALUES
-    ('8.2.2', 'Any act/form of cheating.' , 'c', '14');
 
--- 8.2.3
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.3', 'Acts of immoral conduct and disrespect.' , 'c', '14');
+    ('Recruiting to any fraternities and/or sororities.' , 'c', '14');
 
--- 8.2.4
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.4', 'Blatantly disregarding approved school policies.' , 'c', '14');
+    ('Any act/form of cheating.' , 'c', '14');
 
--- 8.2.5
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.5', 'Acts of dishonor to the school.' , 'c', '14');
+    ('Acts of immoral conduct and disrespect.' , 'c', '14');
 
--- 8.2.6
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.6', 'Acts of dishonor to the school.' , 'c', '14');
+    ('Blatantly disregarding approved school policies.' , 'c', '14');
 
--- 8.2.7
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.7', 'Commiting act of cyberbullying.' , 'c', '14');
+    ('Acts of dishonor to the school.' , 'c', '14');
 
--- 8.2.8
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.8', 'Acts of Intellectual Property theft.' , 'c', '14');
+    ('False representation as student organizations.' , 'u', '14');
 
--- 8.2.9
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.9', 'False claim of Intellectual Property.' , 'c', '14');
+    ('Commiting act of cyberbullying.' , 'c', '14');
 
--- Minor (p)
--- 8.1.1
-INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
-VALUES
-    ('8.1.1', 'Using foul language or gestures.' , 'p', '07');
 
--- 8.1.2
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.2', 'Showing disrespect to National Symbols.' , 'p', '07');
+    ('Acts of Intellectual Property theft.' , 'c', '14');
 
--- 8.1.3
-INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
-VALUES
-    ('8.1.3', 'Showing inappropriate behavior.' , 'p', '07');
 
--- 8.1.4
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.1.4', 'Inappropriate display of intimacy.' , 'p', '07');
+    ('False claim of Intellectual Property.' , 'c', '14');
 
--- Major (p)
--- 8.2.1
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.1', 'Recruiting to any fraternities and/or sororities.' , 'p', '14');
+    ('Using foul language or gestures.' , 'p', '07');
 
--- 8.2.2
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.2', 'Any act/form of cheating.' , 'p', '14');
+    ('Showing disrespect to National Symbols.' , 'p', '07');
 
--- 8.2.3
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.3', 'Acts of immoral conduct and disrespect.' , 'p', '14');
+    ('Showing inappropriate behavior.' , 'p', '07');
 
--- 8.2.4
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.4', 'Blatantly disregarding approved school policies.' , 'p', '14');
+    ('Inappropriate display of intimacy.' , 'p', '07');
 
--- 8.2.5
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.5', 'Acts of dishonor to the school.' , 'p', '14');
+    ('Recruiting to any fraternities and/or sororities.' , 'p', '14');
 
--- 8.2.6
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.6', 'Acts of dishonor to the school.' , 'p', '14');
+    ('Any act/form of cheating.' , 'p', '14');
 
--- 8.2.7
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.7', 'Commiting act of cyberbullying.' , 'p', '14');
+    ('Acts of immoral conduct and disrespect.' , 'p', '14');
 
--- 8.2.8
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.8', 'Acts of Intellectual Property theft.' , 'p', '14');
+    ('Blatantly disregarding approved school policies.' , 'p', '14');
 
--- 8.2.9
 INSERT INTO offenses    
-    (offense_id, offense_title, offense_type, ban_time)
+    (offense_title, offense_type, ban_time)
 VALUES
-    ('8.2.9', 'False claim of Intellectual Property.' , 'p', '14');
+    ('Acts of dishonor to the school.' , 'p', '14');
+
+INSERT INTO offenses    
+    (offense_title, offense_type, ban_time)
+VALUES
+    ('False representation as student organizations.' , 'u', '14');
+
+INSERT INTO offenses    
+    (offense_title, offense_type, ban_time)
+VALUES
+    ('Commiting act of cyberbullying.' , 'p', '14');
+
+INSERT INTO offenses    
+    (offense_title, offense_type, ban_time)
+VALUES
+    ('Acts of Intellectual Property theft.' , 'p', '14');
+
+INSERT INTO offenses    
+    (offense_title, offense_type, ban_time)
+VALUES
+    ('False claim of Intellectual Property.' , 'p', '14');
