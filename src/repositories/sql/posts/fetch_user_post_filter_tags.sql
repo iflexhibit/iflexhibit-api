8db0ad420edbbc6d3c3a0c1fbe87d6d885a3fb12
LyndonTomas <201801180@iacademy.edu.ph>
2021-12-09 10:38:41 +0800

    adds filter tags on fetch user post

--- src/repositories/sql/posts/fetch_user_post_filter_tags.sql
+++ src/repositories/sql/posts/fetch_user_post_filter_tags.sql
@@ -25,8 +25,9 @@ SELECT
 FROM posts
 JOIN users ON posts.user_id = users.user_id
 JOIN poststatus ON posts.status_id = poststatus.status_id
-WHERE posts.status_id = 2 AND posts.is_deleted = FALSE AND posts.user_id = $1
+WHERE posts.status_id = 2 AND posts.is_deleted = FALSE AND posts.user_id = $1 AND post_tags LIKE $3
 ORDER BY comments_count DESC;
 
 -- $1 user's id
 -- $2 page number;
+-- $3 tags separated by `%`
