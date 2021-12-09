6bc9f6ee79f6169a92db0f4350666c9dbdc5a0e4
LyndonTomas <201801180@iacademy.edu.ph>
2021-12-09 12:25:34 +0800

    updated logic in fetch all guest search post title

--- src/repositories/sql/posts/fetch_all_guest_search_post_title.sql
+++ src/repositories/sql/posts/fetch_all_guest_search_post_title.sql
@@ -25,10 +25,10 @@ posts.created_at
 FROM posts
 JOIN poststatus ON posts.status_id=poststatus.status_id
 JOIN users ON posts.user_id=users.user_id
-WHERE posts.status_id=2 AND posts.is_deleted=FALSE AND post_title LIKE $2
+WHERE posts.status_id=2 AND posts.is_deleted=FALSE AND LOWER(post_title) = LOWER($2)
 ORDER BY post_title DESC
 LIMIT 15
 OFFSET ($1-1)*15;
 
 -- $1 is the page number
--- $2 is the tags separated with `%`
+-- $2 is the post_title
