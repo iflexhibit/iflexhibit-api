574898ad24818b4e8c7e576406098bb41498e136
LyndonTomas <201801180@iacademy.edu.ph>
2021-12-12 21:31:32 +0800

    updates rules for add_like, add_view, and add_comments

--- src/repositories/sql/rules/rules.sql
+++ src/repositories/sql/rules/rules.sql
@@ -1,12 +1,17 @@
 -- add like
 
 CREATE RULE add_like AS
-	ON INSERT TO userpost
+	ON UPDATE TO userpost
 	DO ALSO
-		UPDATE posts SET likes_count = likes_count +
+		UPDATE posts SET likes_count = 
 			CASE
-				WHEN new.is_liked = TRUE THEN 1
-				ELSE 0
+				WHEN new.is_liked = TRUE THEN 
+					(SELECT COUNT(*)
+        				FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
+        				FROM userpost) + 1
+				ELSE (SELECT COUNT(*)
+        				FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
+        				FROM userpost)
 			END
 		WHERE new.post_id = posts.post_id;
 
@@ -15,7 +20,11 @@ CREATE RULE add_like AS
 CREATE RULE add_view AS 
 	ON INSERT TO userpost
 	DO ALSO
-		UPDATE posts SET views_count = views_count + 1
+		UPDATE posts SET views_count = (
+        SELECT COUNT(*)
+        FILTER (WHERE posts.post_id=userpost.post_id)
+        FROM userpost 
+    	) + 1
 		WHERE new.post_id = posts.post_id;
 
 --add comments
@@ -23,5 +32,9 @@ CREATE RULE add_view AS
 CREATE RULE add_comments AS
 	ON INSERT TO comments
 	DO ALSO
-		UPDATE posts SET comments_count = comments_count + 1
+		UPDATE posts SET comments_count = (
+        SELECT COUNT(*)
+        FILTER (WHERE posts.post_id=comments.post_id AND comments.is_disabled=FALSE AND comments.is_deleted=FALSE)
+        FROM comments
+    	) + 1
 		WHERE new.post_id = posts.post_id;
