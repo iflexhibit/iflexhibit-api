fafd49593df02a67e3d9c08d6355e3b92f0182a5
LyndonTomas <201801180@iacademy.edu.ph>
2021-12-12 10:53:36 +0800

    updated fetch user post sql command

--- src/repositories/sql/posts/fetch_user_posts.sql
+++ src/repositories/sql/posts/fetch_user_posts.sql
@@ -4,67 +4,35 @@ SELECT
     posts.user_id,
     users.username,
     users.avatar_image,
-    (
-        SELECT COUNT(*)
-        FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
-        FROM userpost
-    ) as likes_count,
-    (
-        SELECT COUNT(*)
-        FILTER (WHERE posts.post_id=userpost.post_id)
-        FROM userpost 
-    ) AS views_count,
-    (
-        SELECT COUNT(*)
-        FILTER (WHERE posts.post_id=comments.post_id AND comments.is_disabled=FALSE AND comments.is_deleted=FALSE)
-        FROM comments
-    ) AS comments_count,
+    likes_count,
+    views_count,
+    comments_count,
     posts.post_title,
+        posts.post_tags,
     posts.post_image,
     posts.created_at
 FROM posts
 JOIN users ON posts.user_id = users.user_id
 JOIN poststatus ON posts.status_id = poststatus.status_id
 WHERE posts.status_id = 2 AND posts.is_deleted = FALSE AND posts.user_id = $1
-ORDER BY posts.created_at DESC;
+AND (CASE
+            WHEN $5 = 4 THEN posts.post_title ~* $4
+            WHEN $5 = 5 THEN posts.post_tags LIKE $3
+            ELSE TRUE
+END)
+ORDER BY CASE
+    WHEN $5 = 1 THEN likes_count
+    WHEN $5 = 2 THEN views_count
+    WHEN $5 = 3 THEN comments_count
+    ELSE post_id
+END DESC
+LIMIT 15
+OFFSET ($2-1)*15;
 
--- $1 user's id
--- $2 page number;
-
--- New test code as of 12-10-21
-CASE
-    WHEN 1 = 1 THEN
-        (SELECT 
-        posts.post_id,
-        posts.status_id,
-        posts.user_id,
-        users.username,
-        users.avatar_image,
-        (
-            SELECT COUNT(*)
-            FILTER (WHERE userpost.is_liked AND posts.post_id=userpost.post_id)
-            FROM userpost
-        ) as likes_count,
-        (
-            SELECT COUNT(*)
-            FILTER (WHERE posts.post_id=userpost.post_id)
-            FROM userpost 
-        ) AS views_count,
-        (
-            SELECT COUNT(*)
-            FILTER (WHERE posts.post_id=comments.post_id AND comments.is_disabled=FALSE AND comments.is_deleted=FALSE)
-            FROM comments
-        ) AS comments_count,
-        posts.post_title,
-        posts.post_image,
-        posts.created_at
-        FROM posts
-        JOIN users ON posts.user_id = users.user_id
-        JOIN poststatus ON posts.status_id = poststatus.status_id
-        WHERE posts.status_id = 2 AND posts.is_deleted = FALSE AND posts.user_id = $1
-        ORDER BY likes_count DESC;)
-    WHEN THEN
-    WHEN THEN
-    WHEN THEN
-    ELSE
-END
+-- SYNTAX ($1 user_id, $2 pagenumber, $3 tags_pattern, $4 title pattern, $5 order_type)
+-- Types of order type ($5) : 
+-- 1 - order thru likes
+-- 2 - order thru views
+-- 3 - order thru comments
+-- 4 - add title param
+-- 5 - add tags param
