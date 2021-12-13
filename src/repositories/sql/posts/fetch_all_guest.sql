fee50cd784e40a315e876eee834b232f419cccb4
LyndonTomas <201801180@iacademy.edu.ph>
2021-12-13 13:06:09 +0800

    updated order of paramaeters and its docs

--- src/repositories/sql/posts/fetch_all_guest.sql
+++ src/repositories/sql/posts/fetch_all_guest.sql
@@ -15,21 +15,19 @@ FROM posts
 JOIN users ON posts.user_id = users.user_id
 JOIN poststatus ON posts.status_id = poststatus.status_id
 WHERE posts.status_id = 2 AND posts.is_deleted = FALSE
-AND posts.post_title ~* $3 AND posts.post_tags LIKE $2
+AND posts.post_title ~* $1 AND posts.post_tags LIKE $2
 ORDER BY CASE
-    WHEN $4 = 1 THEN likes_count
-    WHEN $4 = 2 THEN views_count
-    WHEN $4 = 3 THEN comments_count
+    WHEN $3 = 1 THEN likes_count
+    WHEN $3 = 2 THEN views_count
+    WHEN $3 = 3 THEN comments_count
     ELSE post_id
 END DESC
 LIMIT 15
-OFFSET ($1-1)*15;
+OFFSET ($4-1)*15;
 
 -- end of code
--- SYNTAX ($1 pagenumber, $2 tags_pattern, $3 title pattern, $4 order_type)
--- Types of order type ($4) : 
+-- SYNTAX ($1 post_title, $2 post_tags, $3 order_pattern, $4 page_number)
+-- Types of order type ($3) : 
 -- 1 - order thru likes
 -- 2 - order thru views
 -- 3 - order thru comments
--- 4 - add title param
--- 5 - add tags param
