70acf41b9c5d962f56272bb89040bf15597689c7
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-14 22:42:00 +0800

    updates syntax

--- src/repositories/sql/usertypes/_data.sql
+++ src/repositories/sql/usertypes/_data.sql
@@ -1,15 +1,15 @@
 -- guests
-INSERT INTO usertypes (usertype_id, usertype_title)
-VALUES ('ut0', 'guest');
+INSERT INTO usertypes (usertype_id, permission_id, usertype_title)
+VALUES ('ut0', 'gue', 'guest');
 -- member
-INSERT INTO usertypes (usertype_id, usertype_title)
-VALUES ('ut1', 'member');
+INSERT INTO usertypes (usertype_id, permission_id, usertype_title)
+VALUES ('ut1', 'mem', 'member');
 -- moderator
-INSERT INTO usertypes (usertype_id, usertype_title)
-VALUES ('ut2', 'moderator');
+INSERT INTO usertypes (usertype_id, permission_id, usertype_title)
+VALUES ('ut2', 'mod', 'moderator');
 -- administrator
-INSERT INTO usertypes (usertype_id, usertype_title)
-VALUES ('ut3', 'administrator');
+INSERT INTO usertypes (usertype_id, permission_id, usertype_title)
+VALUES ('ut3', 'adm', 'administrator');
 -- banned
-INSERT INTO usertypes (usertype_id, usertype_title)
-VALUES ('ut4', 'banned');
+INSERT INTO usertypes (usertype_id, permission_id, usertype_title)
+VALUES ('ut4', 'ban', 'banned');
