99fcb0c899ca458afdca1d8328eda039ae7740e2
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-14 22:50:10 +0800

    removes alter_table.sql

--- src/repositories/sql/posts/alter_post.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE posts
-ADD COLUMN likes_count INT DEFAULT 0,
-ADD COLUMN comments_count INT DEFAULT 0,
-ADD COLUMN views_count INT DEFAULT 0;
-
--- This command adds a likes, comments, and views counter per post.
