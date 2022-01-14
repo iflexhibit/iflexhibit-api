ae8ef9d4dc106e57dd4f1d6ea40b07b5c721aafc
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-14 22:24:02 +0800

    updates _data.sql in usertypes

--- src/repositories/sql/usertypes/_data.sql
+++ src/repositories/sql/usertypes/_data.sql
@@ -1,15 +1,15 @@
 -- guests
-INSERT INTO usertypes (permission_id, usertype_title)
+INSERT INTO usertypes (usertype_id, usertype_title)
 VALUES ('ut0', 'guest');
 -- member
-INSERT INTO usertypes (permission_id, usertype_title)
+INSERT INTO usertypes (usertype_id, usertype_title)
 VALUES ('ut1', 'member');
 -- moderator
-INSERT INTO usertypes (permission_id, usertype_title)
+INSERT INTO usertypes (usertype_id, usertype_title)
 VALUES ('ut2', 'moderator');
 -- administrator
-INSERT INTO usertypes (permission_id, usertype_title)
+INSERT INTO usertypes (usertype_id, usertype_title)
 VALUES ('ut3', 'administrator');
 -- banned
-INSERT INTO usertypes (permission_id, usertype_title)
+INSERT INTO usertypes (usertype_id, usertype_title)
 VALUES ('ut4', 'banned');
