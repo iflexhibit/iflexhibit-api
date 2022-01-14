fe5c08fb4cfdc5236c3cdd89c508388fa5d43dba
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-14 21:51:27 +0800

    updates id convention of poststatus

--- src/repositories/sql/poststatus/_table.sql
+++ src/repositories/sql/poststatus/_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE poststatus (
-    "status_id" SMALLSERIAL NOT NULL PRIMARY KEY UNIQUE,
+    "status_id" VARCHAR(3) NOT NULL PRIMARY KEY,
     "status_title" VARCHAR(10) NOT NULL DEFAULT 'Pending',
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
