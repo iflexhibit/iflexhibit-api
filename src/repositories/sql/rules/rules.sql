6fb9d257d6f0d46a1ecd1e8ef41b6ec8deb0d3cb
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-15 22:53:00 +0800

    updates add_view rule

--- src/repositories/sql/rules/rules.sql
+++ src/repositories/sql/rules/rules.sql
@@ -22,7 +22,7 @@ CREATE RULE add_view AS
 	DO ALSO
 		UPDATE posts SET views_count = (
         SELECT COUNT(*)
-        FILTER (WHERE posts.post_id=userpost.post_id)
+        FILTER (WHERE posts.post_id = userpost.post_id AND posts.user_id = userpost.user_id)
         FROM userpost 
     	) + 1
 		WHERE new.post_id = posts.post_id;
