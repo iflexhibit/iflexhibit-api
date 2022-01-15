a62642f5409a42707bb6756958fbe4a414c99fab
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-15 12:04:39 +0800

    updates data type reference of permission_id

--- src/repositories/sql/usertypes/_table.sql
+++ src/repositories/sql/usertypes/_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE usertypes (
     "usertype_id" VARCHAR(3) NOT NULL PRIMARY KEY,
-    "permission_id" VARCHAR(3) NOT NULL REFERENCES permissions(permission_id) DEFAULT 'gue',
+    "permission_id" CHAR(3) NOT NULL REFERENCES permissions(permission_id) DEFAULT 'gue',
     "usertype_title" VARCHAR(20) NOT NULL DEFAULT 'NO ACCESS',
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
