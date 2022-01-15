1974f4e5ee78c5a1adf2132400abfe25f6ba2999
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-15 12:03:57 +0800

    updates data type of permission_id

--- src/repositories/sql/permissions/_table.sql
+++ src/repositories/sql/permissions/_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE permissions (
-    "permission_id" VARCHAR(3) PRIMARY KEY,
+    "permission_id" CHAR(3) PRIMARY KEY,
     "submit_post" BOOLEAN NOT NULL DEFAULT FALSE,
     "comment_post" BOOLEAN NOT NULL DEFAULT FALSE,
     "moderator_access" BOOLEAN NOT NULL DEFAULT FALSE,
