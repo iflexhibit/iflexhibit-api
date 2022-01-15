d61a485008393915f46cf3cd6e2b9f15e63fa024
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-15 12:06:37 +0800

    updates data type of status_id

--- src/repositories/sql/poststatus/_table.sql
+++ src/repositories/sql/poststatus/_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE poststatus (
-    "status_id" VARCHAR(3) NOT NULL PRIMARY KEY,
+    "status_id" CHAR(3) NOT NULL PRIMARY KEY,
     "status_title" VARCHAR(10) NOT NULL DEFAULT 'Pending',
     "created_at" TIMESTAMPTZ DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
