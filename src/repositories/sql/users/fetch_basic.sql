30edfc8e10d7f9cf704de18ecb40549b6febdcef
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-16 11:19:10 +0800

    arranged spacing in fetch basic

--- src/repositories/sql/users/fetch_basic.sql
+++ src/repositories/sql/users/fetch_basic.sql
@@ -4,4 +4,4 @@ username,
 A.avatar_image,
 A.created_at
 FROM users
-A INNER JOIN usertypes B ON A.usertype_id=B.usertype_id WHERE user_id=$1;
+A INNER JOIN usertypes B ON A.usertype_id = B.usertype_id WHERE user_id = $1;
