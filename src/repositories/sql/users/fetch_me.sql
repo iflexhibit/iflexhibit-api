cd64f8e5dddc0c09d1204bfbd1259e5b06da889b
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-17 11:25:41 +0800

    updates code; subject for testing

--- src/repositories/sql/users/fetch_me.sql
+++ src/repositories/sql/users/fetch_me.sql
@@ -1,4 +1,5 @@
-SELECT user_id,
+SELECT 
+    user_id,
     usertype_title,
     username,
     given_name,
@@ -11,7 +12,12 @@ SELECT user_id,
     show_name,
     show_email,
     show_contact,
+    permissions.submit_post,
+    permissions.comment_post,
+    permissions.moderator_access,
+    permissions.admin_access,
     A.created_at
 FROM users
 A INNER JOIN usertypes B ON A.usertype_id = B.usertype_id
+usertypes INNER JOIN permissions ON usertypes.permission_id = permissions.permission_id
 WHERE user_id = $1;
