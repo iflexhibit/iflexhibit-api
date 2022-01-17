9ed2da8bcb156332f75b34155223dc46bd6d120b
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-17 22:39:13 +0800

    removes spaces in drop.sql

--- src/repositories/sql/drop.sql
+++ src/repositories/sql/drop.sql
@@ -1,52 +1,29 @@
 -- Rules
-
 -- add like
 DROP RULE add_like ON userpost;
 -- add view
 DROP RULE view ON userpost;
 -- add comments
 DROP RULE add_comments ON comments;
-
 -- Bans
-
 DROP TABLE bans;
-
 -- Reports
-
 DROP TABLE reports;
-
 -- Userpost
-
 DROP TABLE userpost;
-
 -- Comments
-
 DROP TABLE comments;
-
 -- Posts
-
 DROP TABLE posts;
-
 -- Users
-
 DROP TABLE users;
-
 -- Usertypes
-
 DROP TABLE usertypes;
-
 -- Permissions
-
 DROP TABLE permissions;
-
 -- Poststatus
-
 DROP TABLE poststatus;
-
 -- Offenses
-
 DROP TABLE offenses;
-
 -- Session
-
 DROP TABLE session;
