2e3dfa593df875decf466d1ebddf075ae80dfb0d
LyndonTomas <201801180@iacademy.edu.ph>
2022-01-22 17:39:50 +0800

    writes testing findings

--- src/repositories/sql/userpost/like_one.sql
+++ src/repositories/sql/userpost/like_one.sql
@@ -6,4 +6,4 @@ WHERE userpost.post_id = $1 AND userpost.user_id = $2;
 --  $1 post_id (the post being liked)
 -- $2 user_id (the user liking the post)
 -- Status: In Progress; Subject for Testing
--- Findings: (1-22-22)
+-- Findings: Working (1-22-22)
