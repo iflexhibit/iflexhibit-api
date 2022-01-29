939e815f7d39c37dc2e2a89c5f2fb817995e5952
John Paul Ong <jpong5202@gmail.com>
2022-01-29 15:53:17 +0800

    Update report sql command format and add comments

--- src/repositories/sql/reports/report_user.sql
+++ src/repositories/sql/reports/report_user.sql
@@ -3,4 +3,7 @@ INSERT INTO reports
 VALUES
     ($1, NULL, NULL, $2, $3, $4);
 
--- user to user
+-- $1 - ID of the user being reported
+-- $2 - ID of the user submitting the report
+-- $3 - Offense ID
+-- $4 - Report Note
