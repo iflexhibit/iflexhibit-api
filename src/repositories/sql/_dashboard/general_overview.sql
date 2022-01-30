73694825bc700623e309d41faf07246bf0d427c8
LyndonTomas <61931218+LyndonTomas@users.noreply.github.com>
2022-01-30 14:59:10 +0800

    Dashboard SQL - General Overview data #121 (#123)
    * first attempt in adding the rules
    
    * finalizes query for reported users, posts, and comments
    
    * compiles all required parameters in a view
    
    * removes comments above
    
    * creates 2nd version of query
    
    * adds drop function for general_overview in drop.sql
    
    * adds create function for general_overview in create.sql
    
    * removes select command in create.sql

---
+++ src/repositories/sql/_dashboard/general_overview.sql
@@ -0,0 +1,34 @@
+DROP VIEW IF EXISTS general_overview;
+
+CREATE VIEW general_overview AS
+    SELECT 
+        (SELECT COUNT (DISTINCT target_user_id) FROM reports) AS reported_users,
+        (SELECT COUNT (DISTINCT target_post_id) FROM reports) AS reported_posts,
+        (SELECT COUNT (DISTINCT target_comment_id) FROM reports) AS reported_comments,
+        (SELECT COUNT (target_id) FROM bans WHERE bans.expires_at > now()) AS banned_users,
+        (SELECT COUNT (post_id) FROM posts WHERE posts.status_id = 'ps1') AS pending_posts,
+        (SELECT COUNT (post_id) FROM posts WHERE posts.status_id = 'ps4') AS disabled_posts,
+        (SELECT COUNT (comment_id) FROM comments WHERE is_disabled = TRUE) AS disabled_comments,
+        (SELECT now()) AS issued_at;
+SELECT * FROM general_overview;
+
+-- Syntax
+
+-- To get all 
+-- SELECT * FROM general_overview;
+
+-- To get specific value
+-- SELECT * FROM general_overview.VALUE HERE BASED from the ALIASES ABOVE;
+
+
+-- Also this version works
+
+-- SELECT 
+--         (SELECT COUNT (DISTINCT target_user_id) FROM reports) AS reported_users,
+--         (SELECT COUNT (DISTINCT target_post_id) FROM reports) AS reported_posts,
+--         (SELECT COUNT (DISTINCT target_comment_id) FROM reports) AS reported_comments,
+--         (SELECT COUNT (target_id) FROM bans WHERE bans.expires_at > now()) AS banned_users,
+--         (SELECT COUNT (post_id) FROM posts WHERE posts.status_id = 'ps1') AS pending_posts,
+--         (SELECT COUNT (post_id) FROM posts WHERE posts.status_id = 'ps4') AS disabled_posts,
+--         (SELECT COUNT (comment_id) FROM comments WHERE is_disabled = TRUE) AS disabled_comments,
+--         (SELECT now()) AS issued_at;
--- src/repositories/sql/create.sql
+++ src/repositories/sql/create.sql
@@ -195,3 +195,16 @@ CREATE OR REPLACE RULE count_comments AS
         FILTER (WHERE posts.post_id = comments.post_id AND comments.is_disabled = FALSE AND comments.is_deleted = FALSE)
         FROM comments)
 		WHERE new.post_id = posts.post_id;
+
+-- Create View
+
+CREATE VIEW general_overview AS
+    SELECT 
+        (SELECT COUNT (DISTINCT target_user_id) FROM reports) AS reported_users,
+        (SELECT COUNT (DISTINCT target_post_id) FROM reports) AS reported_posts,
+        (SELECT COUNT (DISTINCT target_comment_id) FROM reports) AS reported_comments,
+        (SELECT COUNT (target_id) FROM bans WHERE bans.expires_at > now()) AS banned_users,
+        (SELECT COUNT (post_id) FROM posts WHERE posts.status_id = 'ps1') AS pending_posts,
+        (SELECT COUNT (post_id) FROM posts WHERE posts.status_id = 'ps4') AS disabled_posts,
+        (SELECT COUNT (comment_id) FROM comments WHERE is_disabled = TRUE) AS disabled_comments,
+        (SELECT now()) AS issued_at;
