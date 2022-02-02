de02d85aaa8d12fd217ff39cfb3ad9219a217934
LyndonTomas <201801180@iacademy.edu.ph>
2022-02-02 22:06:27 +0800

    removes comments

--- src/repositories/sql/_dashboard/reported_users.sql
+++ src/repositories/sql/_dashboard/reported_users.sql
@@ -16,22 +16,3 @@ CREATE VIEW reported_users AS
     ORDER BY reports.created_at ASC;
 
 SELECT * FROM reported_users;
-
--- DROP VIEW IF EXISTS pending_posts;
-
--- CREATE VIEW pending_posts AS
---     SELECT 
---         posts.post_id,
---         posts.user_id,
---         users.username,
---         posts.post_title,
---         posts.post_body,
---         posts.post_image,
---         posts.post_video,
---         posts.created_at
---     FROM posts
---     INNER JOIN users ON users.user_id = posts.user_id
---     WHERE posts.status_id = 'ps1'
---     ORDER BY 
---         created_at DESC;
--- SELECT * FROM pending_posts;
