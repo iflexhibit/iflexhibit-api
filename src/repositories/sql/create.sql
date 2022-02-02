78c7aa34ae146bf148735a7a915c371c89c74532
LyndonTomas <201801180@iacademy.edu.ph>
2022-02-02 23:18:58 +0800

    updates query in reported_users in create.sql

--- src/repositories/sql/create.sql
+++ src/repositories/sql/create.sql
@@ -218,9 +218,10 @@ CREATE VIEW reported_users AS
         reports.user_id,
         (SELECT username FROM users WHERE users.user_id = reports.user_id) AS complainee_username,
         reports.offense_id,
-        (SELECT offense_title FROM offenses WHERE offenses.offense_id = reports.offense_id) AS offense_title,
-        (SELECT ban_time FROM offenses WHERE offenses.offense_id = reports.offense_id),
+        offenses.offense_title,
+        offenses.ban_time,
         reports.report_note,
         reports.created_at
     FROM reports
+    JOIN offenses ON offenses.offense_id = reports.offense_id
     ORDER BY reports.created_at ASC;
