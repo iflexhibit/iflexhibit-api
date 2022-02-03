d2e755b6a6431f69539572f5912889b721bde6d8
LyndonTomas <201801180@iacademy.edu.ph>
2022-02-03 21:58:07 +0800

    solves user_issue

--- src/repositories/sql/_dashboard/reported_posts.sql
+++ src/repositories/sql/_dashboard/reported_posts.sql
@@ -5,7 +5,7 @@ CREATE VIEW reported_posts AS
         reports.report_id,
         reports.target_post_id,
         posts.post_title,
-        posts.user_id,
+        reports.target_user_id,
         (SELECT username FROM users WHERE users.user_id = posts.user_id) AS target_username,
         reports.user_id,
         (SELECT username FROM users WHERE users.user_id = reports.user_id) AS complainee_username,
@@ -21,24 +21,3 @@ CREATE VIEW reported_posts AS
     ORDER BY reports.created_at ASC;
 
 SELECT * FROM reported_posts;
-
--- basis
--- DROP VIEW IF EXISTS reported_users;
-
--- CREATE VIEW reported_users AS
---     SELECT 
---         reports.report_id,
---         reports.target_user_id,
---         (SELECT username FROM users WHERE users.user_id = reports.target_user_id) AS target_username,
---         reports.user_id,
---         (SELECT username FROM users WHERE users.user_id = reports.user_id) AS complainee_username,
---         reports.offense_id,
---         offenses.offense_title,
---         offenses.ban_time,
---         reports.report_note,
---         reports.created_at
---     FROM reports
---     JOIN offenses ON offenses.offense_id = reports.offense_id
---     ORDER BY reports.created_at ASC;
-
--- SELECT * FROM reported_users;
