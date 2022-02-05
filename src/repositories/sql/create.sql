5911b3c70fa20625d1d63903171c688008137015
LyndonTomas <201801180@iacademy.edu.ph>
2022-02-05 20:13:35 +0800

    adds banned_users in create.sql and sql/_dashboard

--- src/repositories/sql/create.sql
+++ src/repositories/sql/create.sql
@@ -246,3 +246,19 @@ CREATE VIEW reported_users AS
     JOIN offenses ON offenses.offense_id = reports.offense_id
     WHERE offenses.offense_type = 'u'
     ORDER BY reports.created_at ASC;
+
+    -- banned_users
+    CREATE VIEW banned_users AS 
+    SELECT 
+        bans.ban_id,
+        bans.target_id,
+        (SELECT username FROM users WHERE users.user_id = bans.target_id) AS banned_user,
+        bans.user_id,
+        (SELECT username FROM users WHERE users.user_id = bans.user_id) AS complainee_username,
+        bans.offense_id,
+        offenses.offense_title,
+        bans.created_at,
+        bans.expires_at
+    FROM bans
+    JOIN offenses ON offenses.offense_id = bans.offense_id
+    ORDER BY complainee_username ASC;
