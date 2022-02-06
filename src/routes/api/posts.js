757104292a4a96bfe1bfe2ca1e4751aa8906bb1f
John Paul Ong <jpong5202@gmail.com>
2022-02-06 18:25:19 +0800

    Add title and description trimmer and limit validation

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -75,6 +75,21 @@ router.post("/", auth, async (req, res) => {
       return res.status(400).json({ status: 400, msg: "Title required" });
     }
 
+    if (!fields.title.trim().length > 0) {
+      fs.unlinkSync(files.file.path);
+      return res.status(400).json({ status: 400, msg: "Title required" });
+    }
+
+    if (fields.description) {
+      if (fields.description.trim().length > 1000) {
+        fs.unlinkSync(files.file.path);
+        return res.status(400).json({
+          status: 400,
+          msg: "Description cannot exceed 1000 characters",
+        });
+      }
+    }
+
     try {
       const response = await cloudinary.uploader.upload(files.file.path, {
         folder: "iflexhibit/uploads",
@@ -109,8 +124,8 @@ router.post("/", auth, async (req, res) => {
 
       const post = await PostRepository.insertPost(
         req.user.id,
-        fields.title,
-        fields.description,
+        fields.title.trim(),
+        fields.description ? fields.description.trim() : null,
         response.url,
         null,
         fields.tags
