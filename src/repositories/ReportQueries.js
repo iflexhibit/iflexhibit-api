8c4e3b811ade335d75c42d9565c684cddd62d33e
John Paul Ong <64911824+jp-ong@users.noreply.github.com>
2022-02-08 15:57:45 +0800

    Add functional fetching of Report Entries data (#149)
    * Initial system dashboard workspace
    
    * Add React import to all files
    
    * Add to static dashboard
    
    * Add initial moderator dashboard
    
    * Remove localhost prefix
    
    * Add cookie config
    
    * Add httpOnly to cookie config
    
    * Change cookie secure to auto
    
    * Merge client package w/ main
    
    * Merge w/ main
    
    * Add reported comments to API endpoint
    
    * Add link to post comment

--- src/repositories/ReportQueries.js
+++ src/repositories/ReportQueries.js
@@ -3,7 +3,7 @@ module.exports = {
   INSERT INTO reports 
     (target_user_id, target_post_id, target_comment_id, user_id, offense_id, report_note)
   VALUES
-    ((SELECT user_id from comments WHERE comment_id=$1), NULL, $1, $2, $3, $4);
+    ((SELECT user_id from comments WHERE comment_id=$1), (SELECT post_id from comments WHERE comment_id=$1), $1, $2, $3, $4);
     `,
   reportPost: `
   INSERT INTO reports 
