c0073d1928bbbc3d49206867ade1d9af41c87d74
LyndonTomas <201801180@iacademy.edu.ph>
2022-02-09 20:03:38 +0800

    removes testing template

--- src/repositories/sql/_dashboard/ban_user.sql
+++ src/repositories/sql/_dashboard/ban_user.sql
@@ -9,9 +9,3 @@ RETURNING ban_id;
 -- $2 user_id
 -- $3 offense_id
 -- $4 ban_note
-
-INSERT INTO bans (report_id, target_id, user_id, offense_id, ban_note, expires_at)
-VALUES ($1, 
-(SELECT target_user_id FROM reports WHERE report_id = $1), $2, $3, $4, 
-(SELECT now() + concat((SELECT ban_time FROM offenses WHERE offense_id = $3), ' days')::INTERVAL))
-RETURNING ban_id;
