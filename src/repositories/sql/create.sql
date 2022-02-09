ca894a9181a313bd7de9afcf29fde4b82ec3b251
LyndonTomas <201801180@iacademy.edu.ph>
2022-02-09 21:55:13 +0800

    adds disabled_comments in create.sql and drop.sql

--- src/repositories/sql/create.sql
+++ src/repositories/sql/create.sql
@@ -309,3 +309,16 @@ CREATE VIEW reported_comments AS
     WHERE offenses.offense_type = 'c'
     ORDER BY reports.created_at ASC;
 
+-- disabled_comments
+CREATE VIEW disabled_comments AS
+    SELECT
+        comments.comment_id,
+        comments.comment_body,
+        comments.user_id,
+        users.username,
+        comments.post_id,
+        comments.created_at
+    FROM comments
+    JOIN users ON users.user_id = comments.user_id
+    WHERE is_disabled = TRUE
+    ORDER BY comments.comment_id ASC;
