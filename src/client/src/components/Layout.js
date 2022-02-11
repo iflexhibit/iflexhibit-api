da7b30444da7119b5ef1c6c2d0ffee7c8ea3a71d
John Paul Ong <jpong5202@gmail.com>
2022-02-11 15:06:32 +0800

    Update router implementation

--- src/client/src/components/Layout.js
+++ src/client/src/components/Layout.js
@@ -1,26 +1,14 @@
-import PendingPostsLayout from "./layouts/PendingPostsLayout";
-import ReportEntriesLayout from "./layouts/ReportEntriesLayout";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import React from "react";
-import GeneralOverviewLayout from "./layouts/GeneralOverviewLayout";
-import BannedEntriesLayout from "./layouts/BannedEntriesLayout";
-import { Route, Routes } from "react-router-dom";
 
-const Layout = () => {
+const Layout = ({ layoutContent }) => {
   return (
     <>
       <Navbar />
       <main>
         <Sidebar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<GeneralOverviewLayout />} />
-            <Route path="/pending" element={<PendingPostsLayout />} />
-            <Route path="/reports" element={<ReportEntriesLayout />} />
-            <Route path="/bans" element={<BannedEntriesLayout />} />
-          </Routes>
-        </div>
+        <div className="content">{layoutContent}</div>
       </main>
     </>
   );
