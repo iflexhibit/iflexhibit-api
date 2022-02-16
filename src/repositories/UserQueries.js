86ef0d02239f619dd4a2c8bad244e051dad08c08
John Paul Ong <jpong5202@gmail.com>
2022-02-16 16:50:29 +0800

    Fix updateUsertype SQL query

--- src/repositories/UserQueries.js
+++ src/repositories/UserQueries.js
@@ -99,5 +99,5 @@ module.exports = {
   deleteComment: `
     UPDATE comments SET is_deleted=TRUE WHERE comment_id=$1 AND user_id=$2 RETURNING comment_id;
   `,
-  updateUsertype: `UPDATE users SET usertype_id = 'ut1' WHERE users.user_id = $1 AND NOT EXISTS(SELECT ban_id FROM bans WHERE bans.target_id = $1 AND (SELECT expires_at FROM bans WHERE bans.target_id = $1) > now());`,
+  updateUsertype: `UPDATE users SET usertype_id = 'ut1' WHERE users.user_id = $1 AND NOT EXISTS(SELECT ban_id FROM bans WHERE bans.target_id = $1 AND (SELECT expires_at FROM bans WHERE bans.target_id = $1 ORDER BY expires_at DESC LIMIT 1) > now());`,
 };
