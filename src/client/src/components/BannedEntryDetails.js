bbcffe56807cdfb614f3e2dde4176acf0fb3a7de
John Paul Ong <jpong5202@gmail.com>
2022-02-16 15:50:55 +0800

    Add new line for ban note

--- src/client/src/components/BannedEntryDetails.js
+++ src/client/src/components/BannedEntryDetails.js
@@ -26,7 +26,15 @@ const BannedEntryDetails = ({ ctx }) => {
       <DetailsGroup label="banned by" value={`${ctx.reporter.username}`} />
       <DetailsGroup label="banned at" value={`${formatDate(ctx.createdAt)}`} />
       <DetailsGroup label="expires at" value={`${formatDate(ctx.expiresAt)}`} />
-      {ctx.note && <DetailsGroup label="ban note" value={ctx.note} />}
+      {ctx.note && (
+        <DetailsGroup
+          label="ban note"
+          value={ctx.note.split("\n").map((p, index) => (
+            <p key={index}>{p}</p>
+          ))}
+        />
+      )}
+
       <Button
         color="blue"
         fullWidth
