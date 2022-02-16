beb1df75815196553cb75615b315502016e334fa
John Paul Ong <jpong5202@gmail.com>
2022-02-16 18:26:16 +0800

    Fix command error

--- src/repositories/UserQueries.js
+++ src/repositories/UserQueries.js
@@ -104,5 +104,5 @@ module.exports = {
   SET usertype_id = 'ut4'
   WHERE users.user_id = $1 
   AND EXISTS(SELECT ban_id FROM bans WHERE bans.target_id = $1 AND 
-  (SELECT expires_at FROM bans WHERE bans.target_id = $1) > now());`,
+  (SELECT expires_at FROM bans WHERE bans.target_id = $1 ORDER BY expires_at DESC LIMIT 1) > now());`,
 };
