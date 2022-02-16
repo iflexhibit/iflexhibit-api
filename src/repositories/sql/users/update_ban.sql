d221068603589b14cfa35ea98ff1a903710b04d7
John Paul Ong <jpong5202@gmail.com>
2022-02-16 18:27:40 +0800

    Update SQL file to reflect changes

--- src/repositories/sql/users/update_ban.sql
+++ src/repositories/sql/users/update_ban.sql
@@ -1,5 +1,5 @@
 UPDATE users 
-SET usertype_id = 'ut4'
-WHERE users.user_id = $1 
-AND EXISTS(SELECT ban_id FROM bans WHERE bans.target_id = $1 AND 
-(SELECT expires_at FROM bans WHERE bans.target_id = $1) > now());
+  SET usertype_id = 'ut4'
+  WHERE users.user_id = $1 
+  AND EXISTS(SELECT ban_id FROM bans WHERE bans.target_id = $1 AND 
+  (SELECT expires_at FROM bans WHERE bans.target_id = $1 ORDER BY expires_at DESC LIMIT 1) > now());
--- src/repositories/sql/users/update_usertype.sql
+++ src/repositories/sql/users/update_usertype.sql
@@ -1,9 +1,4 @@
-UPDATE users 
-SET usertype_id = 'ut1'
-WHERE users.user_id = $1 
-                AND NOT EXISTS(SELECT ban_id 
-                    FROM bans WHERE bans.target_id = $1 AND 
-                    (SELECT expires_at FROM bans WHERE bans.target_id = $1) > now());
+UPDATE users SET usertype_id = 'ut1' WHERE users.user_id = $1 AND NOT EXISTS(SELECT ban_id FROM bans WHERE bans.target_id = $1 AND (SELECT expires_at FROM bans WHERE bans.target_id = $1 ORDER BY expires_at DESC LIMIT 1) > now());
 
 -- Syntax
 -- $1 user_id
