73feff1d3da63163fa440cb3704bec2c33ff1890
John Paul Ong <jpong5202@gmail.com>
2022-02-26 18:47:21 +0800

    Update comment deletion

--- src/repositories/sql/comments/delete_comment.sql
+++ src/repositories/sql/comments/delete_comment.sql
@@ -1,1 +1,5 @@
 UPDATE comments SET is_deleted=TRUE WHERE comment_id=$1 AND user_id=$2 RETURNING comment_id;
+
+-- NEW
+
+DELETE FROM comments WHERE comment_id=$1 AND user_id=$2 RETURNING comment_id;
